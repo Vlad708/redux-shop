b4131be0f37945d667a4a4ae6a5a1d13e0592e66
vladislav.varfolomeev <vlad708.var@gmail.com>
2019-03-05 19:00:01 +0600

    Setting up routing

---
+++ react-shopping-cart/src/components/ProductItem.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+/*import { Menu, Popup, List, Button, Image } from 'semantic-ui-react';*/
+
+class ProductItem extends Component {
+
+  render () {    
+
+    return (
+      <p>Test message</p>
+    )
+  }
+}
+
+export default ProductItem
--- react-shopping-cart/src/index.js
+++ react-shopping-cart/src/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
 import './app.css';
 
 import App from './containers/App';
+import ProductItem from './components/ProductItem';
 
 import createStore from './store';
 const store = createStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+  	<BrowserRouter>    	
+    	<Switch>
+		  <Route exact path='/' component={App} />	 
+		  <Route path='/product' component={ProductItem} />		  
+		</Switch>
+	</BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
