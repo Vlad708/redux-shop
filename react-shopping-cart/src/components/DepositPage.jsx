8ebab780e66cb8b14d7e203388eb49b1b5b574ea
Vlad Varfolomeev <vlad708.var@gmail.com>
2019-03-26 22:41:28 +0600

    Removed paypal package

--- react-shopping-cart/src/components/DepositPage.jsx
+++ react-shopping-cart/src/components/DepositPage.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import PayPalButton from 'react-paypal-button'
 
 class DepositPage extends Component {
 
@@ -7,13 +6,6 @@ class DepositPage extends Component {
 		return (
 			<div>
 				<h1>Выберите метод оплаты</h1>
-				<PayPalButton
-	        env='sandbox'
-	        sandboxID='abcdef123456'
-	        amount='0.01'
-	        currency='USD'
-	        commit={true}
-	      />
 			</div>
 		)
 	}
